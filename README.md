9599dc51b83f694a856faddd7adc2e26156f6b30
Anthony Duong <anthonyduong789@gmail.com>
2026-03-01 19:58:45 -0800

    implementing fuzzy search

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FuzzyClipExtension
+understanding the fuzzy search
+
+- length of the match. Searching for “aa” should score higher for Cars/Saab than Cars/Toyota. (they both match)
+- we implement a way to grade which string ranks higher using the Levenshtein distance algorithm
+
+_Levenshtein distance Properties
+- It is zero if and only if the strings are equal.
+- It is at least the difference of the sizes of the two strings.
+- at most the length of the longer string
+- Triangle inequality: The Levenshtein distance between two strings is no greater than the sum of their Levenshtein distances from a third string.
--- content.js
+++ content.js
@@ -1,13 +1,68 @@
-let html
-
-
-
-
+let html;
+let isInjected = false;
 // Load the file
 fetch(chrome.runtime.getURL('fuzzy.html'))
   .then(response => response.text())
   .then(res => {
-    console.log(res);
-    html = res;
+    html = res
     // document.getElementById('container').innerHTML = html;
   });
+
+// inject html 
+window.addEventListener('load', async () => {
+  const injectedDiv = document.createElement('div');
+  if (html) {
+    injectedDiv.innerHTML = html
+    document.body.appendChild(injectedDiv);
+    const closeButton = document.getElementById('closeInjected');
+    if (closeButton) {
+      closeButton.addEventListener('click', () => {
+        injectedDiv.remove();
+      });
+    }
+
+  }
+  document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'q') {
+      // Toggle the state of isInjected
+      isInjected = !isInjected;
+
+      if (isInjected) {
+        // Append the injectedDiv if it's not already appended
+        if (!document.body.contains(injectedDiv)) {
+          document.body.appendChild(injectedDiv);
+        }
+      } else {
+        // Remove the injectedDiv if it's already appended
+        if (document.body.contains(injectedDiv)) {
+          injectedDiv.remove();
+        }
+      }
+    }
+  })
+});
+
+
+
+/**
+ * Searches through a list of strings for a given query using a fuzzy search algorithm.
+ * @param {string} query - The query to search for.
+ * @param {string[]} list - The list of strings to search through.
+ * @returns {object[]} - An array of objects containing the matched string and its indices in the original list.
+ */
+
+function fuzzySearch(query, list) {
+  const lowerQuery = query.toLowerCase();
+
+}
+
+function LevdistScore(s, t){
+
+
+}
+
+
+
+
+
+
